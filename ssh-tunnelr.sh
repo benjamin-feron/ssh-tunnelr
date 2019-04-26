f20f6fc4669301e0040f3cf0b719a109a295f3a8
benjamin.feron <benjamin.feron@iut-tlse3.fr>
2019-04-27 01:40:59 +0200

    multiple ranges

--- ssh-tunnelr.sh
+++ ssh-tunnelr.sh
@@ -16,6 +16,7 @@ show_help () {
   echo "  -f, --forward        Single port or range to forward to endpoint. You can specify simple port range e.g. 80:82."
   echo "                       You also can specify output port range with a third port number e.g. 7000:7002:80."
   echo "                       So port 7000 will be forwarded on port 80 of the endpoint, 7001 on 81 and 7002 on 82."
+  echo "                       This option can be repeated multiples times e.g -f 110:118 -f 7000:7002:80 -f 3306."
   echo "  -d, --dry            Dry mode, for test. With this option, ssh command is not launched, it's only shown."
   echo "  --help               Show help"
   echo ""
@@ -34,7 +35,7 @@ while :; do
 	  shift
 	  ;;
 	-f|--forward)
-	  PORTS_RANGE=${2}
+	  PORTS_RANGE="$PORTS_RANGE ${2}"
 	  shift
 	  ;;
 	-d|--dry)
@@ -71,12 +72,7 @@ throw_error () {
 
 # unserialize data
 IFS=',' read -r -a HSTS <<< "$HOSTS"
-IFS=':' read -r -a PR   <<< "$PORTS_RANGE"
-
-# if single port specify
-if (( "${#PR[@]}" == 1 )); then
-  PR[1]=PR[0]
-fi
+IFS=' ' read -r -a PRS  <<< "$PORTS_RANGE"
 
 # checks
 if [ "$USERNAME" = "" ]; then
@@ -88,36 +84,52 @@ fi
 if [ "$PORTS_RANGE" = "" ]; then
   throw_error "Please specify port range"
 fi
-if (( "${PR[0]}" > "${PR[1]}" )); then
-  throw_error "Source port must be less than end port"
-fi
-if (( "${PR[0]}" < 1 )) || (( "${PR[1]}" < 1 )) || (( "${#PR[@]}" == 3 )) && (( "${PR[2]}" < 1)); then
-#if (( "${PR[0]}" < 1 )) || (( "${PR[1]}" < 1 )); then
-  throw_error "Ports numbers  must be greater than 1"
-fi
-if (( "${PR[0]}" > $MAX_PORT_NUMBER )) || \
-   (( "${#PR[@]}" > 1 )) && (( "${PR[1]}" > $MAX_PORT_NUMBER )) || \
-   (( "${#PR[@]}" > 2 )) && (( "${PR[2]}" > $MAX_PORT_NUMBER )); then
-  throw_error "Ports numbers must be less than $MAX_PORT_NUMBER"
-fi
 
+# construct ssh command
 CMD=""
 # for each host
 for ((i=0; i<${#HSTS[@]}; ++i)); do
-  CMD="$CMD\nssh -t $USERNAME@${HSTS[$i]}\n"
-  SRC_PORT="${PR[0]}"
-  DST_PORT="${PR[0]}"
-  CNT=0
-  # for each port in range
-  for ((j=${PR[0]}; j<=${PR[1]}; ++j)); do
-  	# if output port is specified
+  CMD="$CMD\nssh $USERNAME@${HSTS[$i]}\n"
+
+  # for each range in ports ranges
+  for ((j=0; j<${#PRS[@]}; ++j)); do
+    # unserialize range
+    IFS=':' read -r -a PR <<< "${PRS[$j]}"
+
+    # if single port specified
+    if (( "${#PR[@]}" == 1 )); then
+      PR[1]=${PR[0]}
+      PR[2]=${PR[0]}
+    # if simple range port specified
+    #elif (( "${#PR[@]}" == 2 )); then
+      #PR[2]=${PR[1]}
+    fi
+
+    SRC_PORT=${PR[0]}
+    DST_PORT=${PR[0]}
+  
+  	# if last host AND out ports range specified
     if [[ "$(( i + 1 ))" -eq "${#HSTS[@]}" && "${#PR[@]}" -eq "3" ]]; then
-	  DST_PORT="$(( ${PR[2]} + $CNT ))"
+	  DST_PORT=${PR[2]}
 	fi
-	CMD="$CMD-L $SRC_PORT:localhost:$DST_PORT\n"
-	(( SRC_PORT++ ))
-	(( DST_PORT++ ))
-	(( CNT++ ))
+
+	# checks
+	if (( $SRC_PORT < 1 )) || (( $DST_PORT < 1 )); then
+	  throw_error "Ports numbers  must be greater than 1"
+	fi
+	if (( ${PR[0]} > ${PR[1]} )); then
+	  throw_error "First port in range must be less than last port"
+	fi
+
+	# for each port in range
+	for ((k=${PR[0]}; k<=${PR[1]}; ++k)); do
+	  if (( $SRC_PORT > $MAX_PORT_NUMBER )) || (( $DST_PORT > $MAX_PORT_NUMBER )); then
+		throw_error "Ports numbers  must be less than $MAX_PORT_NUMBER - $DST_PORT"
+	  fi
+	  CMD="$CMD-L $SRC_PORT:localhost:$DST_PORT\n"
+	  (( SRC_PORT++ ))
+	  (( DST_PORT++ ))
+    done
   done
 done
 
